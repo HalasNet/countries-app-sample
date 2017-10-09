9de6b0376a9bfa0b8be89b757e181d75cbc7958e
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2017-10-09 12:05:39 +0200

    Update runBackend.sh

--- test/runBackend.sh
+++ test/runBackend.sh
@@ -1,7 +1,7 @@
 cd $(dirname $0)
 cd ../countries
 
-mvn clean package
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=noraui -Dsonar.login=$SONAR_TOKEN -Punit-tests
 java -jar target/countries-0.0.1-SNAPSHOT.jar &
 PID=$!
 sleep 15
