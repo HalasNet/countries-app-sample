36151ed1d765aba9e918bcada726af9a347cf957
sgrillon14 <sgrillon@hotmail.fr>
2017-10-10 21:47:08 +0200

    add spring security + Swagger

--- countries/src/main/java/com/github/noraui/countries/WebSecurityConfig.java
+++ countries/src/main/java/com/github/noraui/countries/WebSecurityConfig.java
@@ -13,7 +13,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+        http.authorizeRequests()
+        .antMatchers("/", "/home").permitAll()
+        .antMatchers("/health").hasRole("SUPERUSER")
+        .anyRequest().authenticated().and()
+        .formLogin().loginPage("/login").permitAll().and()
+        .logout().permitAll();
     }
 
     @Autowired
