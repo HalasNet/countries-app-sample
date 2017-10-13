f119c9ea40123a9572079ae8efc7964345edbf09
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2017-10-14 00:55:15 +0200

    Update runBackend.sh

--- test/runBackend.sh
+++ test/runBackend.sh
@@ -8,10 +8,13 @@ PID=$!
 sleep 30
 
 curl --cookie-jar cookie -L http://localhost:8084/health
+echo "* 1 **********************"
+cat cookie
+echo "* 2 **********************"
 cat cookie | grep 'JSESSIONID'
+echo "* 3 **********************"
 
 curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
-
 TOKEN=$(sed -n 's:.*value="\(.*\)".*:\1:p' nonaui.log | head -n 1)
 echo "*********************** $TOKEN"
 
