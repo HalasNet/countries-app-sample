485e5eeb69411bcf4f81a65651bb582880452e71
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2017-11-03 12:55:30 +0100

    Update after_success.sh

--- test/after_success.sh
+++ test/after_success.sh
@@ -5,7 +5,7 @@ echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
     
 if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/countries-app-sample' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    cd project
+    cd countries
     mvn_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
     echo "Maven version is $mvn_version"
     if [[ $mvn_version == *"-SNAPSHOT" ]]; then
